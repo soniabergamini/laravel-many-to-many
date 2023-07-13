26055e45eb86ffb473bf0f75a9d1753c9ace5ab6
soniabergamini <sounia96@outlook.it>
2023-07-13 09:58:13 +0200

    first commit

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $types = ["Social Network", "Fitness", "Design", "Mobile App", "Dynamic Web Application", "Animated Web Application", "Single-page Application", "Basic E-Commerce", "Advanced E-Commerce", "Other"];
+
+        foreach ($types as $item) {
+            $newType = new Type();
+            $newType->name = $item;
+            $newType->save();
+        }
+
+        // for ($x=0; $x < 10; $x++) {
+        //     $type = new Type();
+        //     $type->name = $faker->word();
+        //     $type->save();
+        // }
+    }
+}
