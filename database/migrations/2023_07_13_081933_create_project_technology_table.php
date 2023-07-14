29d325f8e8a5798bbecb054fe15c012e320f38e8
soniabergamini <sounia96@outlook.it>
2023-07-14 15:03:56 +0200

    feat: add onDelete method to foreign key

--- database/migrations/2023_07_13_081933_create_project_technology_table.php
+++ database/migrations/2023_07_13_081933_create_project_technology_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('project_id')->nullable()->constrained();
-            $table->foreignId('technology_id')->nullable()->constrained();
+            $table->foreignId('project_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('technology_id')->nullable()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
